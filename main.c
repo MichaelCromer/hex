99bdf22aa48dda42f00b1c058c0fe31c776f89b5
Michael Cromer <mcromer@proton.me>
2024-09-02 21:43:55 +1000

    Basic continue loop over user input

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
+int CONTINUE = 1;
+int lastchar = 0;
+
 
 int init_screen()
 {
@@ -19,12 +22,16 @@ int draw_screen()
     int rows, cols;
     getmaxyx(stdscr, rows, cols);
 
-    mvhline(0, 0, ACS_HLINE, cols)
+    mvhline(0, 0, ACS_HLINE, cols);
     mvvline(1, 0, ACS_VLINE, rows);
     mvvline(0, cols-1, ACS_VLINE, cols);
     mvhline(rows-1, 0, ACS_HLINE, cols);
 
     mvprintw(1, 1, "Welcome, test");
+    mvprintw(2, 2, "Last char was : ");
+    attron(A_BOLD);
+    printw("%c", lastchar);
+    attroff(A_BOLD);
 
     refresh();
     return 0;
@@ -33,7 +40,15 @@ int draw_screen()
 
 int handle_input()
 {
-    getch();
+    int ch = getch();
+
+    switch (ch) {
+        case 'q':
+            CONTINUE = 0;
+            break;
+        default:
+            lastchar = ch;
+    }
 
     return 0;
 }
@@ -42,8 +57,11 @@ int handle_input()
 int main()
 {
     init_screen();
-    draw_screen();
-    handle_input();
+
+    while (CONTINUE) {
+        draw_screen();
+        handle_input();
+    }
 
     endwin();
     return 0;
