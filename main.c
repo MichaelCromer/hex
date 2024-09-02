b9552594a8d7befb826c6c8e0f1aca4f6258ced8
Michael Cromer <mcromer@proton.me>
2024-09-02 19:35:16 +1000

    Block out basic screen functions

--- main.c
+++ main.c
@@ -1,25 +1,49 @@
 #include <ncurses.h>
+#include <stdlib.h>
 
-int main()
+
+int init_screen()
 {
     initscr();              /* init the lib */
     raw();                  /* disable line buffering */
     keypad(stdscr, TRUE);   /* capture special keys */
     noecho();               /* don't echo user input */
     curs_set(0);            /* disable cursor */
-    
+
+    return 0;
+}
+
+
+int draw_screen()
+{
     int rows, cols;
     getmaxyx(stdscr, rows, cols);
 
-    mvhline(0, 0, ACS_HLINE, cols);
+    mvhline(0, 0, ACS_HLINE, cols)
     mvvline(1, 0, ACS_VLINE, rows);
     mvvline(0, cols-1, ACS_VLINE, cols);
     mvhline(rows-1, 0, ACS_HLINE, cols);
+
     mvprintw(1, 1, "Welcome, test");
 
     refresh();
+    return 0;
+}
 
+
+int handle_input()
+{
     getch();
+
+    return 0;
+}
+    
+    
+int main()
+{
+    init_screen();
+    draw_screen();
+    handle_input();
 
     endwin();
     return 0;
