5d3a10a0038b391f6f0e07165d3d65c65d3a5a25
Michael Cromer <mcromer@proton.me>
2024-09-13 14:19:05 +1000

    Implement panel struct in panel.h

---
+++ panel.h
@@ -0,0 +1,85 @@
+#ifndef PANEL_H
+#define PANEL_H
+
+
+struct Panel {
+    int w, h;
+    int len;
+    char **lines;
+};
+
+
+struct Panel *create_panel(int len)
+{
+    struct Panel *p = malloc(sizeof(struct Panel));
+    if (!p) return NULL;
+
+    p->len = len;
+
+    p->lines = malloc(len * sizeof(char *));
+    if (!p->lines) {
+        free(p);
+        return NULL;
+    }
+
+    for (int i=0; i<len; i++) {
+        p->lines[i] = NULL;
+    }
+
+    p->w = 0;
+    p->h = 0;
+
+    return p;
+}
+
+
+void destroy_panel(struct Panel *p)
+{
+    if (p) {
+        for (int i=0; i<p->len; i++) {
+            free(p->lines[i]);
+        }
+        free(p->lines);
+        free(p);
+    }
+}
+
+
+int add_panel_line(struct Panel *p, const char *line, int index)
+{
+    if (index < 0 || index >= p->len) { return -1; }
+    if (p->lines[index]) { free(p->lines[index]); }
+
+    p->lines[index] = strdup(line);
+    if (!p->lines[index]) { return -1; }
+
+    int len = strlen(line);
+    if ((len+4) > p->w) { p->w = len+4; } /* +4 to fit border and pad */
+    if ((index+5) > p->h) { p->h = index+5; } /* +5 to fit border and pad, 0-indexed */
+
+    return 0;
+}
+
+
+int remove_panel_line(struct Panel *p, int index)
+{
+    if (index < 0 || index >= p->len) { return -1; }
+    if (!p->lines[index]) { return 0; }
+
+    free(p->lines[index]); 
+    p->h = 0;
+    p->w = 0;
+
+    for (int i=0; i<p->len; i++) {
+        if (!p->lines[i]) { continue; }
+
+        int len = strlen(p->lines[i]);
+        if ((len+4) > p->w) { p->w = len+4; }
+        if ((i+5) > p->h) { p->h = i+5; }
+    }
+
+    return 0;
+}
+
+
+#endif
