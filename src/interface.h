252fec5a28b2ab695b3c8b80962cb0f2bd77cca4
Michael Cromer <mcromer@proton.me>
2024-10-13 20:51:19 +1100

    Implement interface source files

---
+++ src/interface.h
@@ -0,0 +1,24 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include <stdbool.h>
+
+#define UI_NUM_PANELS 3
+
+enum UI_PANEL {
+   SPLASH,
+   TERRAIN_SELECTOR,
+   HEX_DETAIL
+};
+
+
+struct UserInterface;
+
+struct UserInterface *ui_create(void);
+struct UserInterface *ui_init(void);
+void ui_destroy(struct UserInterface *ui);
+void ui_toggle(struct UserInterface *ui, enum UI_PANEL p);
+bool ui_show(struct UserInterface *ui, enum UI_PANEL p);
+struct Panel *ui_panel(struct UserInterface *ui, enum UI_PANEL p);
+
+#endif
