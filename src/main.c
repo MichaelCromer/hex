c5101dba0f509a141cfa2292a7f1e6f5aa45ffcd
Michael Cromer <mcromer@proton.me>
2024-11-01 16:02:16 +1100

    Use new key.c/h implementation

--- src/main.c
+++ src/main.c
@@ -9,36 +9,7 @@
 #include "include/panel.h"
 #include "include/draw.h"
 #include "include/interface.h"
-
-
-#define         KEY_TOGGLE_QUIT 'Q'
-#define      KEY_TOGGLE_TERRAIN 'T'
-#define       KEY_TOGGLE_DETAIL 'j'
-
-#define                  KEY_EE 'k'
-#define                  KEY_NE 'i'
-#define                  KEY_NW 'u'
-#define                  KEY_WW 'h'
-#define                  KEY_SW 'n'
-#define                  KEY_SE 'm'
-
-#define          KEY_EE_SPECIAL 'K'
-#define          KEY_NE_SPECIAL 'I'
-#define          KEY_NW_SPECIAL 'U'
-#define          KEY_WW_SPECIAL 'H'
-#define          KEY_SW_SPECIAL 'N'
-#define          KEY_SE_SPECIAL 'M'
-
-#define     KEY_TERRAIN_UNKNOWN '0'
-#define       KEY_TERRAIN_WATER '1'
-#define   KEY_TERRAIN_MOUNTAINS '2'
-#define      KEY_TERRAIN_PLAINS '3'
-#define       KEY_TERRAIN_HILLS '4'
-#define      KEY_TERRAIN_FOREST '5'
-#define      KEY_TERRAIN_DESERT '6'
-#define      KEY_TERRAIN_JUNGLE '7'
-#define       KEY_TERRAIN_SWAMP '8'
-
+#include "include/key.h"
 
 struct StateManager {
     bool quit;
@@ -145,104 +116,6 @@ void cleanup(void)
     endwin();
 }
 
-
-enum DIRECTION key_direction(char ch)
-{
-    switch (tolower(ch)) {
-        case KEY_EE:
-            return EAST;
-        case KEY_NE:
-            return NORTHEAST;
-        case KEY_NW:
-            return NORTHWEST;
-        case KEY_WW:
-            return WEST;
-        case KEY_SW:
-            return SOUTHWEST;
-        case KEY_SE:
-            return SOUTHEAST;
-        default:
-            return DIRECTION_NONE;
-    }
-}
-
-
-bool key_is_direction(char ch)
-{
-    switch (tolower(ch)) {
-        case KEY_EE:
-        case KEY_NE:
-        case KEY_NW:
-        case KEY_WW:
-        case KEY_SW:
-        case KEY_SE:
-            return true;
-        default:
-            return false;
-    }
-}
-
-
-bool key_is_special(char ch)
-{
-    switch (ch) {
-        case KEY_EE_SPECIAL:
-        case KEY_NE_SPECIAL:
-        case KEY_NW_SPECIAL:
-        case KEY_WW_SPECIAL:
-        case KEY_SW_SPECIAL:
-        case KEY_SE_SPECIAL:
-            return true;
-        default:
-            return false;
-    }
-}
-
-
-enum TERRAIN key_terrain(char ch)
-{
-    switch (ch) {
-        case KEY_TERRAIN_UNKNOWN:
-            return TERRAIN_UNKNOWN;
-        case KEY_TERRAIN_WATER:
-            return WATER;
-        case KEY_TERRAIN_MOUNTAINS:
-            return MOUNTAINS;
-        case KEY_TERRAIN_PLAINS:
-            return PLAINS;
-        case KEY_TERRAIN_HILLS:
-            return HILLS;
-        case KEY_TERRAIN_FOREST:
-            return FOREST;
-        case KEY_TERRAIN_DESERT:
-            return DESERT;
-        case KEY_TERRAIN_JUNGLE:
-            return JUNGLE;
-        case KEY_TERRAIN_SWAMP:
-            return SWAMP;
-        default:
-            return TERRAIN_NONE;
-    }
-}
-
-
-enum TERRAIN key_is_terrain(char ch)
-{
-    switch (ch) {
-        case KEY_TERRAIN_UNKNOWN:
-        case KEY_TERRAIN_WATER:
-        case KEY_TERRAIN_MOUNTAINS:
-        case KEY_TERRAIN_PLAINS:
-        case KEY_TERRAIN_HILLS:
-        case KEY_TERRAIN_FOREST:
-        case KEY_TERRAIN_DESERT:
-        case KEY_TERRAIN_JUNGLE:
-        case KEY_TERRAIN_SWAMP:
-            return true;
-        default:
-            return false;
-    }
-}
 
 
 enum INPUTMODE input_parse_capture(void)
