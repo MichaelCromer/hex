2ba6fde10ccb0b3d0e9dc4d33805d7452b55a2c7
Michael Cromer <mcromer@proton.me>
2024-11-18 13:18:01 +1100

    Remove geometry from draw.c

--- src/include/draw.h
+++ src/include/draw.h
@@ -4,17 +4,8 @@
 #include "panel.h"
 #include "grid.h"
 #include "interface.h"
+#include "geometry.h"
 
-struct Geometry;
-
-struct Geometry *geometry_create(float scale, float aspect, int cols, int rows);
-void geometry_destroy(struct Geometry *g);
-void geometry_set_scale(struct Geometry *g, float scale);
-void geometry_set_aspect(struct Geometry *g, float aspect);
-int geometry_cmid(struct Geometry *g);
-int geometry_rmid(struct Geometry *g);
-int geometry_rows(struct Geometry *g);
-int geometry_cols(struct Geometry *g);
 int draw_panel(struct Panel *p);
 int draw_box(int r0, int c0, int w, int h, char bg);
 void draw_ui(struct UserInterface *ui);
