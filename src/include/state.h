92f092e5f406ee91704b20beb5c051a73076af4b
Michael Cromer <mcromer@proton.me>
2024-11-21 16:55:28 +1100

    Create state.[ch] for state manager struct

---
+++ src/include/state.h
@@ -0,0 +1,28 @@
+#ifndef STATE_H
+#define STATE_H
+
+struct StateManager;
+
+struct StateManager *state_create(void);
+void state_initialise(struct StateManager *sm);
+void state_destroy(struct StateManager *s);
+struct Geometry *state_geometry(const struct StateManager *sm);
+struct Map *state_map(const struct StateManager *sm);
+struct UserInterface *state_ui(const struct StateManager *sm);
+enum INPUTMODE state_mode(const struct StateManager *sm);
+void state_set_mode(struct StateManager *sm, enum INPUTMODE mode);
+char *state_charbuf(struct StateManager *sm);
+void state_reset_charbuf(struct StateManager *sm);
+void state_reset_nextchar(struct StateManager *sm);
+void state_set_nextchar(struct StateManager *sm, char c);
+enum UI_COLOUR state_colour(struct StateManager *sm);
+void state_set_colour(struct StateManager *sm, enum UI_COLOUR colour);
+bool state_await(struct StateManager *sm);
+void state_set_await(struct StateManager *sm, bool await);
+bool state_quit(struct StateManager *sm);
+void state_set_quit(struct StateManager *sm, bool quit);
+key state_currkey(struct StateManager *sm);
+void state_set_currkey(struct StateManager *sm, key k);
+void state_set_mode(struct StateManager *sm, enum INPUTMODE mode);
+
+#endif
