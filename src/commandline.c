3ee7a7a0abd6f084847f9a17c9f2c48b6ff7bfc0
Michael Cromer <mcromer@proton.me>
2024-11-28 22:17:27 +1100

    Create commandline.[ch]

---
+++ src/commandline.c
@@ -0,0 +1,208 @@
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "include/commandline.h"
+
+#define COMMANDLINE_BUFFER_SIZE 1024
+#define COMMAND_WORD_QUIT   "quit"
+
+
+struct Command {
+    enum COMMAND_TYPE type;
+    char *data;
+};
+
+
+struct Command *command_create(enum COMMAND_TYPE type, char *data)
+{
+    struct Command *c = malloc(sizeof(struct Command));
+    c->type = type;
+    c->data = data;
+
+    return c;
+}
+
+
+enum COMMAND_TYPE command_type(const struct Command *c)
+{
+    return c->type;
+}
+
+
+const char *command_str(const struct Command *c)
+{
+    return c->data;
+}
+
+
+void command_destroy(struct Command *c)
+{
+    if (c->data) {
+        free(c->data);
+        c->data = NULL;
+    }
+    free(c);
+}
+
+
+struct Commandline {
+    char buffer[COMMANDLINE_BUFFER_SIZE];
+    int len;
+    char *curr;
+};
+
+
+struct Commandline *commandline_create(void)
+{
+    struct Commandline *c = malloc(sizeof(struct Commandline));
+    commandline_reset(c);
+    return c;
+}
+
+
+void commandline_reset(struct Commandline *c)
+{
+    memset(c->buffer, '\0', COMMANDLINE_BUFFER_SIZE);
+    c->len = 0;
+    c->curr = c->buffer;
+}
+
+
+void commandline_destroy(struct Commandline *c)
+{
+    free(c);
+}
+
+
+const char *commandline_str(struct Commandline *c)
+{
+    return c->buffer;
+}
+
+
+void commandline_putch(struct Commandline *c, char ch)
+{
+    if (c->len >= COMMANDLINE_BUFFER_SIZE - 1) {
+        return;
+    }
+
+    c->buffer[++(c->len)] = ch;
+}
+
+
+char commandline_popch(struct Commandline *c)
+{
+    if (c->len <= 0) {
+        return '\0';
+    }
+    char ch = c->buffer[(c->len)-1];
+    c->buffer[(c->len)--] = '\0';
+    return ch;
+}
+
+
+char *commandline_start(struct Commandline *c)
+{
+    return (c->curr = c->buffer);
+}
+
+
+char *commandline_end(struct Commandline *c)
+{
+    return (c->curr = c->buffer + c->len);
+}
+
+
+char *commandline_next(struct Commandline *c)
+{
+    if (c->curr >= c->buffer + c->len + 1) {
+        return c->buffer + c->len + 1;
+    }
+    return (c->curr++);
+}
+
+
+char *commandline_prev(struct Commandline *c)
+{
+    if (c->curr <= c->buffer) {
+        return c->buffer;
+    }
+    return (c->curr--);
+}
+
+
+char *commandline_curr(struct Commandline *c)
+{
+    return c->curr;
+}
+
+
+enum COMMAND_TYPE commandline_parse_type(struct Commandline *c)
+{
+    char *c0 = commandline_curr(c);
+    while (isspace(*c0) && (*c0 != '\0')) {
+        c0 = commandline_next(c);
+    }
+
+    char *c1 = commandline_curr(c);
+    while (!isspace(*c1) && (*c1 != '\0')) {
+        c1 = commandline_next(c);
+    }
+
+    size_t L = c1 - c0;
+
+    if (L == 0) {
+        return COMMAND_TYPE_NONE;
+    }
+
+    if ((L <= strlen(COMMAND_WORD_QUIT)) && (strncmp(COMMAND_WORD_QUIT, c0, L) == 0)) {
+        return COMMAND_TYPE_QUIT;
+    }
+
+    return COMMAND_TYPE_ERROR;
+}
+
+
+char *commandline_parse_data(struct Commandline *c)
+{
+    char *c0 = commandline_curr(c);
+    while (isspace(*c0) && (*c0 != '\0')) {
+        c0 = commandline_next(c);
+    }
+
+    char *c1 = commandline_curr(c);
+    while (!isspace(*c1) && (*c1 != '\0')) {
+        c1 = commandline_next(c);
+    }
+
+    int L = c1 - c0;
+    char *data = malloc(L + 1);
+
+    for (int i = 0; i < L; i++) {
+        data[i] = *(c0 + i);
+    }
+    data[L] = '\0';
+
+    return data;
+}
+
+
+struct Command *commandline_parse(struct Commandline *c)
+{
+    commandline_start(c);
+
+    enum COMMAND_TYPE t = commandline_parse_type(c);
+    
+    switch (t) {
+        case COMMAND_TYPE_QUIT:
+        case COMMAND_TYPE_ERROR:
+            return command_create(t, NULL);
+        case COMMAND_TYPE_EDIT:
+        case COMMAND_TYPE_WRITE:
+        default:
+            break;
+    }
+
+    return command_create(COMMAND_TYPE_NONE, NULL);
+}
---
+++ src/include/commandline.h
@@ -0,0 +1,30 @@
+#ifndef COMMANDLINE_H
+#define COMMANDLINE_H
+
+#include "enum.h"
+
+struct Command;
+struct Commandline;
+
+struct Command *command_create(enum COMMAND_TYPE type, char *data);
+enum COMMAND_TYPE command_type(const struct Command *c);
+const char *command_str(const struct Command *c);
+void command_destroy(struct Command *c);
+
+void commandline_reset(struct Commandline *c);
+struct Commandline *commandline_create(void);
+void commandline_destroy(struct Commandline *c);
+const char *commandline_str(struct Commandline *c);
+void commandline_putch(struct Commandline *c, char ch);
+char commandline_popch(struct Commandline *c);
+char *commandline_start(struct Commandline *c);
+char *commandline_end(struct Commandline *c);
+char *commandline_next(struct Commandline *c);
+char *commandline_prev(struct Commandline *c);
+char *commandline_curr(struct Commandline *c);
+enum COMMAND_TYPE commandline_parse_type(struct Commandline *c);
+char *commandline_parse_data(struct Commandline *c);
+struct Command *commandline_parse(struct Commandline *c);
+
+
+#endif
