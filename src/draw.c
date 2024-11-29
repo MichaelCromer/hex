0814170561f477461c34f782e005c567620870ff
Michael Cromer <mcromer@proton.me>
2024-11-29 15:56:27 +1100

    Migrate draw to use WINDOWs

--- src/draw.c
+++ src/draw.c
@@ -16,38 +16,32 @@
  *      DRAW 01 - Basic shapes
  */
 
-int draw_border(int r0, int c0, int w, int h)
+void wdraw_border(WINDOW *win, int r0, int c0, int w, int h)
 {
-    mvhline(r0,     c0,     ACS_HLINE, w-1);
-    mvvline(r0,     c0,     ACS_VLINE, h-1);
-    mvvline(r0,     c0+w-1, ACS_VLINE, h-1);
-    mvhline(r0+h-1, c0,     ACS_HLINE, w-1);
+    mvwhline(win, r0,     c0,     ACS_HLINE, w-1);
+    mvwvline(win, r0,     c0,     ACS_VLINE, h-1);
+    mvwvline(win, r0,     c0+w-1, ACS_VLINE, h-1);
+    mvwhline(win, r0+h-1, c0,     ACS_HLINE, w-1);
 
-    mvaddch(r0,     c0,     ACS_PLUS);
-    mvaddch(r0,     c0+w-1, ACS_PLUS);
-    mvaddch(r0+h-1, c0,     ACS_PLUS);
-    mvaddch(r0+h-1, c0+w-1, ACS_PLUS);
-
-    return 0;
+    mvwaddch(win, r0,     c0,     ACS_PLUS);
+    mvwaddch(win, r0,     c0+w-1, ACS_PLUS);
+    mvwaddch(win, r0+h-1, c0,     ACS_PLUS);
+    mvwaddch(win, r0+h-1, c0+w-1, ACS_PLUS);
 }
 
 
-int draw_rectangle(int r0, int c0, int w, int h, char bg)
+void wdraw_rectangle(WINDOW *win, int r0, int c0, int w, int h, char bg)
 {
     for (int r=0; r<h; r++) {
-        mvhline(r0+r, c0, bg, w);
+        mvwhline(win, r0+r, c0, bg, w);
     }
-
-    return 0;
 }
 
 
-int draw_box(int r0, int c0, int w, int h, char bg)
+void wdraw_box(WINDOW *win, int r0, int c0, int w, int h, char bg)
 {
-    draw_rectangle(r0, c0, w, h, bg);
-    draw_border(r0, c0, w, h);
-
-    return 0;
+    wdraw_rectangle(win, r0, c0, w, h, bg);
+    wdraw_border(win, r0, c0, w, h);
 }
 
 
@@ -55,28 +49,27 @@ int draw_box(int r0, int c0, int w, int h, char bg)
  *      DRAW 02 - Panels
  */
 
-int draw_panel(struct Panel *p)
+void wdraw_panel(WINDOW *win, struct Panel *p)
 {
     int r = panel_row(p), c = panel_col(p);
     int w = panel_width(p), h = panel_height(p);
-    draw_box(r, c, w, h, ' ');
+    wdraw_box(win, r, c, w, h, ' ');
 
-    for (int i=0; i<panel_len(p); i++) {
+    for (int i = 0; i < panel_len(p); i++) {
         char *line = panel_line(p, i);
-        if (!line) { continue; }
-        mvprintw(r+2+i, c+2, "%s", line);
+        if (!line) {
+            continue;
+        }
+        mvwprintw(win, r+2+i, c+2, "%s", line);
     }
-    return 0;
 }
 
 
-int clear_panel(struct Panel *p)
+void wclear_panel(WINDOW *win, struct Panel *p)
 {
     int r = panel_row(p), c = panel_col(p);
     int w = panel_width(p), h = panel_height(p);
-    draw_rectangle(r, c, w, h, ' ');
-
-    return 0;
+    wdraw_rectangle(win, r, c, w, h, ' ');
 }
 
 /*
@@ -85,7 +78,7 @@ int clear_panel(struct Panel *p)
 
 
 
-void draw_reticule(struct Geometry *g)
+void wdraw_reticule(WINDOW *win, struct Geometry *g)
 {
     char ch = '#';
     int dh = 0;
@@ -96,18 +89,18 @@ void draw_reticule(struct Geometry *g)
 
     attron(COLOR_PAIR(COLOR_RED));
 
-    mvvline(rmid-h_half + 1, cmid-w_half, ch, 2*h_half + 1);
-    mvvline(rmid-h_half + 1, cmid+w_half, ch, 2*h_half + 1);
+    mvwvline(win, rmid-h_half + 1, cmid-w_half, ch, 2*h_half + 1);
+    mvwvline(win, rmid-h_half + 1, cmid+w_half, ch, 2*h_half + 1);
 
-    mvvline(rmid-h_half + 1, cmid-w_half+1, ch, 2*h_half + 1);
-    mvvline(rmid-h_half + 1, cmid+w_half-1, ch, 2*h_half + 1);
+    mvwvline(win, rmid-h_half + 1, cmid-w_half+1, ch, 2*h_half + 1);
+    mvwvline(win, rmid-h_half + 1, cmid+w_half-1, ch, 2*h_half + 1);
 
     for (int col = -w_half; col <= w_half; col++) {
         dh = (col < 0)
             ? floor((w_half+col)*slope)
             : floor((w_half-col)*slope);
-        mvaddch(rmid - (h_half + dh), cmid + col, ch);
-        mvaddch(rmid + (h_half + dh)+1, cmid + col, ch);
+        mvwaddch(win, rmid - (h_half + dh), cmid + col, ch);
+        mvwaddch(win, rmid + (h_half + dh)+1, cmid + col, ch);
     }
 
     attroff(COLOR_PAIR(COLOR_RED));
@@ -115,7 +108,7 @@ void draw_reticule(struct Geometry *g)
 }
 
 
-int draw_hex(struct Geometry *g, struct Hex *hex, int r0, int c0)
+int wdraw_hex(WINDOW *win, struct Geometry *g, struct Hex *hex, int r0, int c0)
 {
     int w_half = (geometry_hex_w(g)+1) / 2,
         h_half = (geometry_hex_h(g)+1) / 2;
@@ -133,7 +126,7 @@ int draw_hex(struct Geometry *g, struct Hex *hex, int r0, int c0)
 
         for (int r = -(h_half + dh)+1; r <= (h_half + dh); r++) {
             ch = terrain_getch(t, c, r, s);
-            mvaddch(r0 + r, c0 + c, ch);
+            mvwaddch(win, r0 + r, c0 + c, ch);
         }
     }
     attroff(COLOR_PAIR(terrain_colour(t)));
@@ -141,7 +134,7 @@ int draw_hex(struct Geometry *g, struct Hex *hex, int r0, int c0)
 }
 
 
-void draw_map(struct Geometry *g, struct Map *map)
+void wdraw_map(WINDOW *win, struct Geometry *g, struct Map *map)
 {
     struct Hex *centre = map_curr(map);
 
@@ -177,7 +170,7 @@ void draw_map(struct Geometry *g, struct Map *map)
                 v = hex_v(hex);
                 dc = round(geometry_scale(g) * (u - u0));
                 dr = round(geometry_scale(g) * geometry_aspect(g) * (v - v0));
-                draw_hex(g, hex, geometry_rmid(g) + dr, geometry_cmid(g) + dc);
+                wdraw_hex(win, g, hex, geometry_rmid(g) + dr, geometry_cmid(g) + dc);
             }
             coordinate_shift(target, EAST);
         }
@@ -194,26 +187,26 @@ void draw_map(struct Geometry *g, struct Map *map)
  */
 
 
-void draw_ui(struct UserInterface *ui)
+void wdraw_ui(WINDOW *win, struct UserInterface *ui)
 {
     for (int p=0; p<NUM_UI_PANELS; p++) {
         if (ui_show(ui, p)) {
-            draw_panel(ui_panel(ui, p));
+            wdraw_panel(win, ui_panel(ui, p));
         }
     }
     return;
 }
 
 
-void draw_statusline(struct State *s)
+void wdraw_statusline(WINDOW *win, struct State *s)
 {
     int r0 = geometry_rows(state_geometry(s))-1,
         c0 = 0,
         w  = geometry_cols(state_geometry(s))-1;
 
-    mvhline(r0, c0, ' ', w);
+    mvwhline(win, r0, c0, ' ', w);
     attron(COLOR_PAIR(state_mode_colour(s)));
-    mvaddstr(r0, c0+1, state_mode_name(s));
+    mvwaddstr(win, r0, c0+1, state_mode_name(s));
     attroff(COLOR_PAIR(state_mode_colour(s)));
 
     switch (state_mode(s)) {
@@ -244,11 +237,9 @@ void draw_statusline(struct State *s)
 
 void draw_state(struct State *s)
 {
-    struct Geometry *g = state_geometry(s);
-
-    draw_map(g, state_map(s));
-    draw_reticule(g);
-    draw_ui(state_ui(s));
-    draw_statusline(s);
+    wdraw_map(state_window(s), state_geometry(s), state_map(s));
+    wdraw_reticule(state_window(s), state_geometry(s));
+    wdraw_ui(state_window(s), state_ui(s));
+    wdraw_statusline(state_window(s), s);
     return;
 }
