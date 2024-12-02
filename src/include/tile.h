0625dde1cc0083ba8c2c5ddde1995113d111cfde
Michael Cromer <mcromer@proton.me>
2024-12-02 22:32:22 +1100

    Add more river functions to tile.[ch]

--- src/include/tile.h
+++ src/include/tile.h
@@ -25,6 +25,8 @@ void tile_toggle_road(struct Tile *tile, enum DIRECTION d);
 void tile_clear_roads(struct Tile *tile);
 bool tile_river(const struct Tile *t, enum DIRECTION d);
 void tile_set_river(struct Tile *t, enum DIRECTION d, bool b);
+void tile_toggle_river(struct Tile *tile, enum DIRECTION d);
+void tile_clear_rivers(struct Tile *tile);
 char tile_getch(struct Tile *tile, int x, int y);
 
 #endif
