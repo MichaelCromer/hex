d0dda70835da17b38400c1678fac5c4f019ef06e
Michael Cromer <mcromer@proton.me>
2024-12-24 16:16:28 +1100

    Add struct State atlas-related helper fns

--- src/include/state.h
+++ src/include/state.h
@@ -13,6 +13,8 @@ void state_update(struct State *state);
 void state_destroy(struct State *state);
 struct Geometry *state_geometry(const struct State *state);
 struct Atlas *state_atlas(const struct State *state);
+void state_set_atlas(struct State *state, struct Atlas *atlas);
+void state_clear_atlas(struct State *state);
 struct UserInterface *state_ui(const struct State *state);
 struct Commandline *state_commandline(const struct State *state);
 enum MODE state_mode(const struct State *state);
