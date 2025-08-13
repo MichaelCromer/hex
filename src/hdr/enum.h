7c4291e910a8bb60aa9d008f194f07beda453eb8
Michael Cromer <mcromer@proton.me>
2025-08-13 11:43:04 +1000

    Clean up enum.h

--- src/hdr/enum.h
+++ src/hdr/enum.h
@@ -3,7 +3,9 @@
 
 #include <stdbool.h>
 
-enum COLOUR_PAIR {
+
+enum COLOUR_PAIR
+{
     COLOUR_PAIR_RED = 1,
     COLOUR_PAIR_GREEN,
     COLOUR_PAIR_YELLOW,
@@ -16,10 +18,13 @@ enum COLOUR_PAIR {
     COLOUR_PAIR_MOUNTAIN,
     COLOUR_PAIR_FOREST,
 };
+
 void colour_initialise(void);
 
+
 #define NUM_UI_PANELS 13
-enum UI_PANEL {
+enum UI_PANEL
+{
     PANEL_SPLASH,
     PANEL_DETAIL,
     PANEL_HINT,
@@ -37,7 +42,8 @@ enum UI_PANEL {
 };
 
 
-enum MODE {
+enum MODE
+{
     MODE_CAPTURE,
     MODE_NAVIGATE,
     MODE_TERRAIN,
@@ -51,6 +57,7 @@ enum MODE {
     MODE_COMMAND,
     MODE_NONE,
 };
+
 const char *mode_name(enum MODE m);
 int mode_colour(enum MODE m);
 bool mode_is_await(enum MODE m);
@@ -59,7 +66,8 @@ enum UI_PANEL mode_panel(enum MODE m);
 
 #define NUM_TERRAIN 9
 #define NUM_TERRAIN_CHOPTS 24
-enum TERRAIN {
+enum TERRAIN
+{
     TERRAIN_NONE,
     TERRAIN_UNKNOWN,
     TERRAIN_WATER,
@@ -78,15 +86,19 @@ int terrain_colour(enum TERRAIN t, char c);
 bool terrain_impassable(enum TERRAIN t);
 const char *terrain_statusline(void);
 
-enum LOCATION {
+
+enum LOCATION
+{
     LOCATION_NONE,
     LOCATION_SETTLEMENT,
     LOCATION_FEATURE,
     LOCATION_DUNGEON
 };
 
+
 #define NUM_DIRECTIONS 6
-enum DIRECTION {
+enum DIRECTION
+{
     DIRECTION_EE,
     DIRECTION_NE,
     DIRECTION_NW,
@@ -95,12 +107,15 @@ enum DIRECTION {
     DIRECTION_SE,
     DIRECTION_XX
 };
+
 enum DIRECTION direction_opposite(enum DIRECTION d);
 enum DIRECTION direction_next(enum DIRECTION d);
 enum DIRECTION direction_prev(enum DIRECTION d);
 
+
 #define NUM_CHILDREN 9
-enum CHILDREN {
+enum CHILDREN
+{
     CHILD0,
     CHILD1,
     CHILD2,
@@ -113,7 +128,8 @@ enum CHILDREN {
 };
 
 
-enum COMMAND {
+enum COMMAND
+{
     COMMAND_NONE,
     COMMAND_ERROR,
     COMMAND_QUIT,
@@ -122,7 +138,8 @@ enum COMMAND {
 };
 
 
-enum STATUS {
+enum STATUS
+{
     STATUS_OK,
     STATUS_SUCCESS_WRITE,
     STATUS_SUCCESS_EDIT_NEW,
@@ -130,6 +147,7 @@ enum STATUS {
     STATUS_ERROR_WRITE,
     STATUS_ERROR_EDIT,
 };
+
 const char *status_string(enum STATUS s);
 
 #endif
